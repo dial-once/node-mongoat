7fb04a8705540f9c5df08cb8656f5bc464487e60
Yacine Khatal <khatal.yacine@gmail.com>
2015-11-03 18:39:42 +0100

    Add beforeAfter method

--- helpers/utils-helper.js
+++ helpers/utils-helper.js
@@ -9,6 +9,30 @@ var Utils = {
     });
 
     return promises;
+  },
+
+  beforeAfter: function (opType, opName, hooks, callback) {
+    var promisedCallback = function (query) {
+      return new Promise(function(resolve, reject) {
+        return resolve(callback(query));
+      });
+    };
+
+    if (opType === 'before') {
+      if (hooks.before[opName] === undefined) {
+        hooks.before[opName] = [];
+      }
+      
+      hooks.before[opName].push(promisedCallback);
+    } else if (opType === 'after') {
+      if (hooks.after[opName] === undefined) {
+        hooks.after[opName] = [];
+      }
+
+      hooks.after[opName].push(promisedCallback);
+    }
+
+    return hooks;
   }
 };
 
