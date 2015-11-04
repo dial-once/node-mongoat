e8720292584e094f1186e4f8c441f3d7f679703d
Yacine Khatal <khatal.yacine@gmail.com>
2015-11-04 16:17:13 +0100

    Add spec for after insert hooks

--- spec/lib/insert-spec.js
+++ spec/lib/insert-spec.js
@@ -65,7 +65,7 @@ describe('Insert', function() {
     });
 
     // test multiple before insert hooks
-    it('should insert new document to Person collection and handle before insert hook',
+    it('should insert new document to Person collection and handle before insert hooks',
     function (done) {
       // add before insert hooks
       _this.testCol.before('insert', function (object) {
@@ -75,6 +75,63 @@ describe('Insert', function() {
 
       _this.testCol.before('insert', function (object) {
         object.company = 'Dial Once';
+        return object;
+      });
+
+      _this.testCol.insert({
+        firstName: 'Yacine',
+        lastName: 'KHATAl',
+        age: 25
+      }).then(function (mongObject) {
+        expect(typeof mongObject).toBe('object');
+        expect(typeof mongObject.result).toBe('object');
+        expect(mongObject.result.ok).toBe(1);
+        expect(mongObject.result.n).toBe(1);
+        done();
+      });
+    });
+  });
+
+  describe('Insert with after hooks', function() {
+    // test only one before insert hook
+    it('should insert new document to Person collection and handle after insert hook',
+    function (done) {
+      // add after insert hook
+      _this.testCol.after('insert', function (object) {
+        expect(object.firstName).toBe('Yacine');
+        expect(object.lastName).toBe('KHATAl');
+        expect(object.age).toBe(25);
+        return object;
+      });
+
+      _this.testCol.insert({
+        firstName: 'Yacine',
+        lastName: 'KHATAl',
+        age: 25
+      }).then(function (mongObject) {
+        expect(typeof mongObject).toBe('object');
+        expect(typeof mongObject.result).toBe('object');
+        expect(mongObject.result.ok).toBe(1);
+        expect(mongObject.result.n).toBe(1);
+        done();
+      });
+    });
+
+    // test multiple after insert hooks
+    it('should insert new document to Person collection and handle after insert hooks',
+    function (done) {
+      // add before insert hooks
+      _this.testCol.after('insert', function (object) {
+         expect(object.firstName).toBe('Yacine');
+        expect(object.lastName).toBe('KHATAl');
+        expect(object.age).toBe(25);
+        return object;
+      });
+
+      _this.testCol.after('insert', function (object) {
+        expect(object.firstName).toBe('Yacine');
+        expect(object.lastName).toBe('KHATAl');
+        expect(object.age).toBe(25);
         return object;
       });
 
