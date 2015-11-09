eaa109e6f55fa5b0629fc5758c607c43ff134b03
Yacine Khatal <khatal.yacine@gmail.com>
2015-11-09 17:23:37 +0100

    Remove useless function

--- helpers/utils-helper.js
+++ helpers/utils-helper.js
@@ -18,23 +18,6 @@ var Utils = {
     return promises;
   },
 
-  setBeforeOrAfter: function (opType, opName, hooks, callback) {
-    var promisedCallback = function (query) {
-      return new Promise(function(resolve, reject) {
-        reject = reject || null; // to avoid jshint unused code
-        return resolve(callback(query));
-      });
-    };
-
-    if (opType === 'before') {
-      hooks.before[opName].push(promisedCallback);
-    } else if (opType === 'after') {
-      hooks.after[opName].push(promisedCallback);
-    }
-
-    return hooks;
-  },
-
   setDatetime: function (opName, datetime, document, options) {
     if (datetime) {
       if (opName === 'update') {
